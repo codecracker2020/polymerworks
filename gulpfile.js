3fd9a9932ec3b598a71fec5f9231b71f4cf62ca6
naveen <naveen.kolla@ge.com>
2017-11-09 12:11:19 +0530

    Gulp Implementation
    Gulp implemented. Run by giving Gulp Serve

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var plugins = require('gulp-load-plugins')();
+
+const livereloadRegex = /\n<script>.+?(?=livereload\.js).+?<\/script>\n/;
+const connect = require('gulp-connect');
+function getTask(task) {
+    return require('./gulp-tasks/' + task)(gulp, plugins);
+}
+gulp.task('watch', () => {
+    livereload.listen();
+    gulp.watch(paths.app.images, ['images']);
+    gulp.watch(paths.app.html, ['html']);
+    gulp.watch(paths.app.clientScripts, ['lint:client']);
+});
+gulp.task('sass', function () {
+    return gulp.src('demo/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('demo'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('compile:index', getTask('compile.index'));
+gulp.task('compile:sass');
+gulp.task('serve', ['compile:sass', 'compile:index'], function () {
+    connect.server({
+        root: 'demo',
+        port: 8887,
+        livereload: true
+    });
+});
